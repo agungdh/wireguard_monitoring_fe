f7a95b1532624f00f8d17827441c0d48c91528aa
AgungDH <agungdh@live.com>
2024-12-31 01:52:02 +0700

    WIP: get data from API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {WireguardService} from './wireguard.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'wireguard-monitor';
+
+  constructor(private wireguardService: WireguardService) {
+    this.wireguardService.getWireguardStat()
+  }
 }
